4a790164d8e43cd50a92d303018f1d6f4bb1b9f8
sssddd44046882 <71304551+sssddd44046882@users.noreply.github.com>
2020-12-01 11:47:27 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://sssddd44046882.github.io/frog302/model.json';
 
 
 function preload() {
